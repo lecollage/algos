3238b9267c512e90b493b63dee934f57ea954cf6
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2023-08-23 00:14:07 +0200

    I success;

--- I-processors.go
+++ I-processors.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"os"
 	"sort"
@@ -27,7 +28,60 @@ var tasksCount int
 var processors []Processor = make([]Processor, 0)
 var tasks []Task = make([]Task, 0)
 
-var busyTimes = make([]int, 0)
+// HEAP
+
+// An FreeProcessors is a min-heap of ints.
+type FreeProcessors []int
+
+func (h FreeProcessors) Len() int           { return len(h) }
+func (h FreeProcessors) Less(i, j int) bool { return h[i] < h[j] }
+func (h FreeProcessors) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *FreeProcessors) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *FreeProcessors) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// Busy
+type BusyProcessor struct {
+	index int
+	till  int
+}
+
+type BusyProcessors []BusyProcessor
+
+func (h BusyProcessors) Len() int { return len(h) }
+func (h BusyProcessors) Less(i, j int) bool {
+	if h[i].till == h[j].till {
+		return h[i].index < h[j].index
+	}
+
+	return h[i].till < h[j].till
+}
+func (h BusyProcessors) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *BusyProcessors) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(BusyProcessor))
+}
+
+func (h *BusyProcessors) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 /*
 4 7
@@ -44,8 +98,6 @@ var busyTimes = make([]int, 0)
 
 func readHeader() {
 	fmt.Fscan(inI, &processorsCount)
-
-	busyTimes = make([]int, processorsCount)
 
 	fmt.Fscan(inI, &tasksCount)
 }
@@ -79,30 +131,65 @@ func readTasks() {
 	}
 }
 
-func sortProcessorsByEnergy() {
+func prepare() {
 	sort.Slice(processors, func(i, j int) bool {
 		return processors[i].energy < processors[j].energy
 	})
-
-	for i := 0; i < processorsCount; i++ {
-		busyTimes[i] = 0
-	}
 }
 
-func applyTasksToProcessors() int {
-	totalCost := 0
+func executeTasks() int {
+	totalEnergy := 0
 
-	for _, task := range tasks {
-		for i := 0; i < processorsCount; i++ {
-			if busyTimes[i] <= task.from {
-				busyTimes[i] = task.to
-				totalCost += task.time * processors[i].energy
-				break
-			}
-		}
+	free := &FreeProcessors{}
+	heap.Init(free)
+
+	for i := range processors {
+		heap.Push(free, i)
 	}
 
-	return totalCost
+	//fmt.Println("free:", *free)
+	//fmt.Println("INIT IS DONE")
+
+	busy := &BusyProcessors{}
+	heap.Init(busy)
+
+	for _, task := range tasks {
+		//fmt.Println()
+		if len(*busy) == 0 {
+			freeProcIndex := heap.Pop(free).(int)
+			//fmt.Println("freeProcIndex:", freeProcIndex)
+			heap.Push(busy, BusyProcessor{index: freeProcIndex, till: task.to})
+
+			totalEnergy += task.time * processors[freeProcIndex].energy
+		} else {
+			// pop all that should be finished at this time
+			for (*busy)[0].till <= task.from {
+				proc := heap.Pop(busy).(BusyProcessor)
+				heap.Push(free, proc.index)
+
+				if len(*busy) == 0 {
+					break
+				}
+
+				//fmt.Println("release proc:", proc, (*busy)[0].till, task.from, len(*busy))
+			}
+
+			if len(*free) > 0 {
+				freeProcIndex := heap.Pop(free).(int)
+				heap.Push(busy, BusyProcessor{index: freeProcIndex, till: task.to})
+
+				totalEnergy += task.time * processors[freeProcIndex].energy
+			} else {
+				// ignore task
+			}
+		}
+
+		//fmt.Println("task:", task)
+		//fmt.Println("busy:", *busy, len(*busy))
+		//fmt.Println("free:", *free, len(*free))
+	}
+
+	return totalEnergy
 }
 
 func main() {
@@ -110,12 +197,12 @@ func main() {
 	readProcessors()
 	readTasks()
 
-	sortProcessorsByEnergy()
+	prepare()
 
 	//fmt.Println("processors: ", processors)
 	//fmt.Println("tasks: ", tasks)
 
-	totalCost := applyTasksToProcessors()
+	totalCost := executeTasks()
 
 	//fmt.Println("processors: ", processors)
 	//fmt.Println("totalCost: ", totalCost)
---
+++ test-heap.go
@@ -0,0 +1,100 @@
+//// This example demonstrates an integer heap built using the heap interface.
+//package main
+//
+//import (
+//	"container/heap"
+//	"fmt"
+//)
+//
+//// An FreeProcessors is a min-heap of ints.
+//type FreeProcessors []int
+//
+//func (h FreeProcessors) Len() int           { return len(h) }
+//func (h FreeProcessors) Less(i, j int) bool { return h[i] < h[j] }
+//func (h FreeProcessors) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+//
+//func (h *FreeProcessors) Push(x any) {
+//	// Push and Pop use pointer receivers because they modify the slice's length,
+//	// not just its contents.
+//	*h = append(*h, x.(int))
+//}
+//
+//func (h *FreeProcessors) Pop() any {
+//	old := *h
+//	n := len(old)
+//	x := old[n-1]
+//	*h = old[0 : n-1]
+//	return x
+//}
+//
+//// Busy
+//type BusyProcessor struct {
+//	index     int
+//	procIndex int
+//	till      int
+//}
+//
+//type BusyProcessors []BusyProcessor
+//
+//func (h BusyProcessors) Len() int { return len(h) }
+//func (h BusyProcessors) Less(i, j int) bool {
+//	fmt.Println("Less >> i, j: ", i, j, h[i], h[j])
+//
+//	if h[i].till == h[j].till {
+//		return h[i].procIndex < h[j].procIndex
+//	}
+//
+//	return h[i].till < h[j].till
+//}
+//func (h BusyProcessors) Swap(i, j int) {
+//	h[i], h[j] = h[j], h[i]
+//	h[i].index = i
+//	h[j].index = j
+//}
+//
+//func (pq *BusyProcessors) Push(x any) {
+//	n := len(*pq)
+//	item := x.(BusyProcessor)
+//	item.index = n
+//	*pq = append(*pq, item)
+//}
+//
+//func (pq *BusyProcessors) Pop() any {
+//	old := *pq
+//	n := len(old)
+//	item := old[n-1]
+//	//old[n-1] = nil  // avoid memory leak
+//	item.index = -1 // for safety
+//	*pq = old[0 : n-1]
+//	return item
+//}
+//
+//// This example inserts several ints into an FreeProcessors, checks the minimum,
+//// and removes them in order of priority.
+//func main() {
+//	//free := &FreeProcessors{1, 0, 2}
+//	//heap.Init(free)
+//	//fmt.Println("init 0: %d\n", (*free)[0])
+//	//
+//	//heap.Push(free, 3)
+//	//
+//	//fmt.Println("free: ", free)
+//	//
+//	//popped := heap.Pop(free).(int)
+//	//
+//	//fmt.Printf("popped: %d\n", popped)
+//	//for free.Len() > 0 {
+//	//	fmt.Printf("%d ", heap.Pop(free))
+//	//}
+//
+//	// busy
+//	busy := &BusyProcessors{}
+//	heap.Init(busy)
+//
+//	heap.Push(busy, BusyProcessor{procIndex: 2, till: 10})
+//	heap.Push(busy, BusyProcessor{procIndex: 1, till: 7})
+//	heap.Push(busy, BusyProcessor{procIndex: 0, till: 14})
+//	heap.Push(busy, BusyProcessor{procIndex: 3, till: 10})
+//
+//	fmt.Println("busy:", *busy)
+//}
