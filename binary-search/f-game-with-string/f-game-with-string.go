da4fbbc7abfe283c2856dd82215adf85c092edf5
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2023-10-03 23:11:10 +0200

    F - partial

--- binary-search/f-game-with-string/f-game-with-string.go
+++ binary-search/f-game-with-string/f-game-with-string.go
@@ -107,28 +107,115 @@ func readInput() (string, string, []int) {
 	return t, p, array
 }
 
-// check if symbols from str1 are included in the same order to str2
-func doesInclude(str1, str2 string) bool {
-	symbols1 := strings.Split(str1, "")
-	symbols2 := strings.Split(str2, "")
+// func getSubstring(t string, exclude []int) string {
+// 	if len(exclude) == 0 {
+// 		return t
+// 	}
 
-	var i = 0
-	for _, symbol1 := range symbols1 {
+// 	result := ""
+
+// 	j := 0
+
+// 	for i := 0; i < len(t); i++ {
+// 		if exclude[j] == i {
+// 			j++
+// 		} else {
+// 			result += string(t[i])
+// 		}
+// 	}
+
+// 	return result
+// }
+
+func modifyString(t string, exclude []int) string {
+	if len(exclude) == 0 {
+		return t
+	}
+
+	result := t
+
+	for _, item := range exclude {
+		result = result[:item-1] + "." + result[item:]
+	}
+
+	return result
+}
+
+// check if symbols from str1 are included in the same order to str2
+/*
+ab.b.ba
+abb
+*/
+func doesInclude(searchSubstring, origString string) bool {
+	symbols1 := strings.Split(searchSubstring, "")
+	symbols2 := strings.Split(origString, "")
+
+	var j = 0
+	for i := 0; i < len(symbols1); i++ {
+		symbol1 := symbols1[i]
 
 	sym2:
-		for ; i < len(symbols2); i++ {
-			if symbols2[i] == symbol1 {
+		for ; j < len(symbols2); j++ {
+			symbol2 := symbols2[j]
+			if i == len(symbols1)-1 && symbol2 == symbol1 {
+				return true
+			}
+
+			if symbol2 == symbol1 {
 				break sym2
 			}
 		}
 	}
 
-	return true
+	return false
 }
 
-func calculate(t, p string, array []int) int {
+/*
+l   m       r
+5 3 4 1 7 6 2
 
-	return 0
+l m   r
+5 3 4 1 7 6 2
+
+a 1
+b 2
+a 3
+b 4
+c 5
+b 6
+a 7
+*/
+func calculate(t, p string, array []int) int {
+	left := 0
+	right := len(t)
+
+	fmt.Println(left, right)
+	out.Flush()
+
+	stop := 20
+
+	for left < right {
+		middle := int((left + right) / 2)
+		lettersToExclude := array[0:middle]
+		modified := modifyString(t, lettersToExclude)
+
+		fmt.Println(modified, lettersToExclude, left, middle, right)
+		out.Flush()
+
+		if doesInclude(p, modified) {
+			right = middle
+		} else {
+			left = middle
+		}
+
+		if stop == 0 {
+			break
+		}
+
+		stop--
+	}
+
+	return right - 1
 }
 
 func main() {
