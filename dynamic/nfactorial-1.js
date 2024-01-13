47ed1fa482f327b0d08b689b4b121d11d4b382c9
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-01-13 15:25:56 +0100

    nfactorial-1

--- dynamic/nfactorial-1.js
+++ dynamic/nfactorial-1.js
@@ -1,3 +1,4 @@
+// Approach 1: simple answer - true/false
 const calc = (target, arr, sum, i) => {
     if(sum === target) {
         return true
@@ -11,6 +12,7 @@ const calc = (target, arr, sum, i) => {
 };
 
 
+// Approach 2: answer - true/false + path >> 
 const calcPathHelper = (target, arr, sum, i) => {
     if (sum === target) {
         return [true, '']
@@ -32,6 +34,36 @@ const calcPath = (target, arr) => {
 
     return [answer, answer ? `${arr[0]}${path}` : '']
 };
+// << answer - true/false + path
+
+// Approach 3: iterative with the array of arrays
+const calcIterative = (target, arr) => {
+    if(!arr.length) {
+        return false
+    }
+
+    const dp = new Array(arr.length)
+    
+    dp[0] = new Array(1)
+
+    dp[0][0] = arr[0]
+
+    for(let i = 1; i < arr.length; i++) {
+        const prevArr = dp[i - 1]
+
+        dp[i] = new Array(prevArr.length * 2)
+
+        for(let j = 0; j < dp[i].length; j++) {
+            const multiplier = j % 2 === 0 ? 1 : -1
+
+            dp[i][j] = dp[i-1][Math.floor(j/2)] + multiplier * arr[i]
+        }
+    }
+
+    // console.log(dp)
+
+    return dp[dp.length - 1].includes(target)
+}
 
 [
     {
@@ -43,17 +75,5 @@ const calcPath = (target, arr) => {
         arr: [9,3,7]
     }
 ].forEach(({target, arr}) => {
-    console.log(calcPath(target, arr));
+    console.log(calcIterative(target, arr));
 })
-
-
-
-/*
-10,15,20,0
-10,15,20,15
-100, 100, 2, 2, 1 ,0
-100, 100, 2, 2, 1 ,0
-100, 100, 3, 2, 1 ,0
-100, 102, 3, 2, 1 ,0
-103, 102, 3, 2, 1 ,0
-*/
