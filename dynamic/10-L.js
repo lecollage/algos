fc35e2de0b7f7e933bce1b35437372deb8d57077
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-01-17 07:35:18 +0100

    10-L; init is wrong

---
+++ dynamic/10-L.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number[][]} arr
+ * @return {number}
+ */
+const calc = (arr) => {
+    if(!arr?.length) {
+        return 0
+    }
+
+    // Arrange
+    const dp = new Array(arr.length)
+
+    for(let i = 0; i < arr.length; i++) {
+      dp[i] = [0, 0]
+    }
+
+    // Init
+    dp[0][0] = 0
+    dp[0][1] = Math.min(...arr[0])
+
+    dp[1][0] = dp[0][1]
+    dp[1][1] = Math.min(...arr[1]) + dp[0][1]
+
+    dp[2][0] = dp[1][1]
+    dp[2][1] = Math.min(...arr[2]) + dp[1][1]
+
+    console.log(dp)
+
+    // Calc
+    for(let i = 3; i < arr.length; i++) {
+      dp[i][0] = dp[i-1][1]
+
+      const A = arr[i][0], B = arr[i-1][1], C = arr[i-2][2]
+
+      dp[i][1] = Math.min(dp[i-1][1] + A, dp[i-2][1] + B, dp[i-3][1] + C)
+    }
+
+    return Math.min(...dp[dp.length - 1])
+}
+
+
+/*
+5
+5 10 15
+2 10 15
+5 5 5
+20 20 1
+20 1 1
+*/
+
+[
+  [
+    [5, 10, 15],
+    [2, 10, 15],
+    [5, 5, 5],
+    [20, 20, 1],
+    [20, 1, 1],
+  ]
+].forEach(arr => {
+    const count = calc(arr)
+    console.log(count);
+    console.log();
+})
