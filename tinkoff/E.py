a51f61e39a690ca3e434764ea43fe79ca6f85cc4
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-02-16 07:22:31 +0100

    E

---
+++ tinkoff/E.py
@@ -0,0 +1,38 @@
+def calc(N: int, K: int, path):
+    if K == 0:
+        print(' '.join([str(i) for i in path]))
+        return
+
+    for i in range(K, N): 
+        calc(i, K-1, [*path, i]) 
+    
+
+N, K = map(int, input().split())
+
+for i in range(K, N+1):
+        calc(i, K-1, [i])
+
+'''
+5 3
+
+
+3 2 1
+
+4 2 1
+4 3 1
+4 3 2
+
+5 4 3
+5 4 2
+5 4 1
+5 3 2
+5 3 1
+5 2 1
+'''
+
+'''
+[3]
+[4]
+[5]
+
+'''
