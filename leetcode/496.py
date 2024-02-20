06456d47105f599084c360ac1a45c92e3cecae0e
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-02-20 07:52:16 +0100

    up

--- leetcode/496.py
+++ leetcode/496.py
@@ -6,22 +6,34 @@ class Solution:
             return []
         
         stack = []
-        output = [0] * len(nums2)
+        dict = {}
 
         for i in range(len(nums2)-1, -1, -1):
             while (stack and nums2[stack[-1]] <= nums2[i]):
                 stack.pop()
             
-            output[i] = stack[-1] if stack else len(nums2)
+            if stack: 
+                dict[nums2[i]] = stack[-1]
+            else: 
+                dict[nums2[i]] = -1
+
             stack.append(i)
             
+        # print(dict)
+
+        output = [0] * len(nums1)
+        for i in range(0, len(nums1)):
+            output[i] = nums2[dict[nums1[i]]] if dict[nums1[i]] >= 0 else -1
+
         return output
 
 
 
 inputs = [
-    [2,-1,5,0,2,3]
+    # [[], [2,-1,5,0,2,3]],
+    [[4,1,2],[1,3,4,2]],
+    [[2,4],[1,2,3,4]],
 ]
 s = Solution()
 for input in inputs:
-    print(s.nextGreaterElement([1], input))
+    print(s.nextGreaterElement(input[0], input[1]))
