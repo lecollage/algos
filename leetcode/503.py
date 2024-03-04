c23e8cd868202973bd2e2dd7fab9330382837cb1
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-03-04 09:02:14 +0100

    84, 503

--- leetcode/503.py
+++ leetcode/503.py
@@ -11,6 +11,15 @@ class Solution:
             while len(stack) > 1 and nums[stack[-1]] <= el:
                 stack.pop()
 
+            stack.append(i)
+        
+
+        for i in range(len(nums)-1, -1, -1):
+            el = nums[i]
+
+            while len(stack) > 1 and nums[stack[-1]] <= el:
+                stack.pop()
+
             if stack[-1] > -1:
                 output[i] = nums[stack[-1]]
 
@@ -22,8 +31,8 @@ class Solution:
 
 
 inputs = [
-    # [1,2,1],
-    [1,2,3,4,3], # [2,3,4,-1,-1]
+    [1,2,1],
+    [1,2,3,4,3], # [2,3,4,-1,-1] -> # [2,3,4,-1,4]
 ]
 s = Solution()
 for input in inputs:
