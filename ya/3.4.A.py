8138ff7b94f0d9c810bc238e7aa9796980e12e32
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-03-09 09:19:21 +0100

    ya/3.4.A

--- ya/3.4.A.py
+++ ya/3.4.A.py
@@ -1,21 +1,19 @@
-    
-def solve(n):    
-    steps = []
+steps = []
 
-    def hanoiTower(n, fromPeg, toPeg):
-        if n == 1: 
-            # print(fromPeg, toPeg)
-            return steps.append([fromPeg, toPeg])
-        unusedPeg = 6 - fromPeg - toPeg
-
-        hanoiTower(n-1, fromPeg, unusedPeg)
+def hanoiTower(n, fromPeg, toPeg):
+    if n == 1: 
         # print(fromPeg, toPeg)
-        steps.append([fromPeg, toPeg])
+        return steps.append([fromPeg, toPeg])
+    unusedPeg = 6 - fromPeg - toPeg
+
+    hanoiTower(n-1, fromPeg, unusedPeg)
+    # print(fromPeg, toPeg)
+    steps.append([fromPeg, toPeg])
+    hanoiTower(n-1, unusedPeg, toPeg)
 
 
 N = int(input())
-solve(N, 1, 3)
-steps = []
+hanoiTower(N, 1, 3)
 
 print(len(steps))
 for step in steps:
