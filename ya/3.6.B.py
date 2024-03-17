8bada85873badcc920b8ec1123456742664020ee
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-03-17 10:21:22 +0100

    3.6.B

---
+++ ya/3.6.B.py
@@ -0,0 +1,63 @@
+from typing import List
+
+'''
+Quick Sort Algorithm
+
+quickSort(arr, beg, end)
+  if (beg < end)
+    pivotIndex = partition(arr,beg, end)
+    quickSort(arr, beg, pivotIndex)
+    quickSort(arr, pivotIndex + 1, end)
+
+partition(arr, beg, end)
+  set end as pivotIndex
+  pIndex = beg - 1
+  for i = beg to end-1
+    if arr[i] < pivot
+        swap arr[i] and arr[pIndex]
+        pIndex++
+    swap pivot and arr[pIndex+1]
+return pIndex + 1
+'''
+
+def partition(arr: List[int], low: int, high: int):
+    pivot = arr[high]
+    i = low-1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+          i+=1
+          arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+
+    return i+1
+        
+
+def quickSort(arr: List[int], low: int, high: int):
+    if low < high:
+        pivotIndex = partition(arr, low, high)
+        quickSort(arr, low, pivotIndex-1)
+        quickSort(arr, pivotIndex+1, high)
+
+    return arr
+
+
+N = int(input())
+arr = list(map(int, input().strip().split()))
+sortedArr = quickSort(arr, 0, len(arr)-1)
+print(' '.join([str(i) for i in sortedArr]))
+
+
+
+'''
+7
+13 17 37 73 31 19 23
+
+4
+18 20 3 17
+
+3
+0 11 0
+'''
+
