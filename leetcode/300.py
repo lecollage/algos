c75c78c2d98960a5236b77f2bb51cc0d66949380
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-05-05 09:05:38 +0200

    300

--- leetcode/300.py
+++ leetcode/300.py
@@ -2,68 +2,112 @@ from typing import List
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        minEl = min(nums)
-        maxEl = max(nums)
-
-        dp = [0 for _ in range(0, maxEl+1)]
+        offset = 10**4
+        dp = [0 for _ in range(0, offset*2+1)]
 
         for i, el in enumerate(nums):
             maxLength = dp[el]
             found = False
 
-            for j in range(el-1, -1, -1):
+            start = el + offset if el >= 0 else el + offset
+
+            # print(start)
+
+            for j in range(start-1, -1, -1):
                 # print(j, el, dp[j], dp[el], maxLength)
                 if dp[j] >= maxLength:
                     maxLength = dp[j]
                     found = True
 
             if found:
-                dp[el] = maxLength + 1
+                dp[start] = maxLength + 1
             else:
-                dp[el] = 1
-            # print(dp)
+                dp[start] = 1
+            # print(el, dp[9998], dp[9999], dp[10000], dp[10001], dp[10002], dp[10003])
 
         return max(dp)
 
+# from typing import List
+
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         maxEl = max(nums)
+
+#         dp = [0 for _ in range(0, maxEl+1)]
+
+#         for i, el in enumerate(nums):
+#             maxLength = dp[el]
+#             found = False
+
+#             for j in range(el-1, -1, -1):
+#                 # print(j, el, dp[j], dp[el], maxLength)
+#                 if dp[j] >= maxLength:
+#                     maxLength = dp[j]
+#                     found = True
+
+#             if found:
+#                 dp[el] = maxLength + 1
+#             else:
+#                 dp[el] = 1
+#             # print(dp)
+
+#         return max(dp)
+
 inputs = [
+    # [
+    #     [1,3,6,7,9,4,10,5,6], # 1,3,6,7,9,10
+    #     6,
+    # ],
+    # [
+    #     [10,9,2,5,3,7,101,18], # 2,5,7,101 | 2,3,5,6
+    #     4,
+    # ],
+    # [
+    #     [5,3,7,9,8], # 5,7,9
+    #     3,
+    # ],
+    # [
+    #     [1,3,2,3],
+    #     3,
+    # ],
+    # [
+    #     [1,8,9,2,6,3,4],
+    #     4,
+    # ],
+    # [
+    #     [7,7,7,7,7,7,7],
+    #     1,
+    # ],
+    # [
+    #     [3,4,2,6],
+    #     3,
+    # ],
+    # [
+    #     [6,4,2],
+    #     1,
+    # ],
+    # [
+    #     [6],
+    #     1,
+    # ],
+    # [
+    #     [1, 2, 0],
+    #     2,
+    # ],
+    # [
+    #     [0,1,2],
+    #     3,
+    # ],
+    # [
+    #     [-1, 0,1,2],
+    #     4,
+    # ],
     [
-        [1,3,6,7,9,4,10,5,6], # 1,3,6,7,9,10
-        6,
-    ],
-    [
-        [10,9,2,5,3,7,101,18], # 2,5,7,101 | 2,3,5,6
-        4,
-    ],
-    [
-        [5,3,7,9,8], # 5,7,9
-        3,
-    ],
-    [
-        [1,3,2,3],
-        3,
-    ],
-    [
-        [1,8,9,2,6,3,4],
-        4,
-    ],
-    [
-        [7,7,7,7,7,7,7],
+        [-1,-2],
         1,
     ],
     [
-        [3,4,2,6],
-        3,
-    ],
-    [
-        [6,4,2],
-        1,
-    ],
-    [
-        [6],
-        1,
-    ],
-    [
-        [1, 2, 0],
+        [-2, -1],
         2,
     ]
 ]
