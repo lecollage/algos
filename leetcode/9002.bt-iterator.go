52abcc6b0f89e5d712b2a8f6219e61a75ff6fb93
Eugene Kalashnikov <eugene.kalashnikov@fadv.com>
2024-08-31 08:58:02 +0200

    up

--- leetcode/9002.bt-iterator.go
+++ leetcode/9002.bt-iterator.go
@@ -2,7 +2,6 @@ package main
 
 import (
 "fmt"
-"math"
 )
 
 
@@ -15,22 +14,47 @@ type TreeNode struct {
 
 // @lc code=start
 type BSTIterator struct {
-    
+	arr []int
 }
 
 
 func Constructor(root *TreeNode) BSTIterator {
-    
+	stack := []*TreeNode{}
+	current := root
+	arr := []int{}
+
+	for current != nil || len(stack) > 0 {
+		// if current != nil  {
+		// 	arr = append(arr, current.Val)
+		// }
+
+		if current != nil  {
+			stack = append(stack, current)
+			current = current.Left
+		} else {
+			current = stack[len(stack)-1]
+			arr = append(arr, current.Val)
+			stack = stack[:len(stack)-1]
+			current = current.Right
+		}
+	}
+
+    return BSTIterator{
+		arr: arr,
+	}
 }
 
 
 func (this *BSTIterator) Next() int {
-    
+	ret := this.arr[0]
+	this.arr = this.arr[1:]
+
+	return ret
 }
 
 
 func (this *BSTIterator) HasNext() bool {
-    
+    return len(this.arr) > 0
 }
 // @lc code=end
 
@@ -38,19 +62,33 @@ func (this *BSTIterator) HasNext() bool {
 func main1() {
 	tree := &TreeNode{
 		Val: 1,
-		Left: nil,
-		Right: &TreeNode{
-			Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+				Left: nil,
+				Right: nil,
+			},
 			Right: nil,
-			Left: nil,
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val: 5,
+				Left: nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val: 6,
+				Left: nil,
+				Right: nil,
+			},
 		},
 	}
 
-	obj := Constructor(root);
-	param_1 := obj.Next();
-	param_2 := obj.HasNext();
+	obj := Constructor(tree);
 
-	fmt.Println(isValidBST(tree))
+	fmt.Println(obj.Next(), obj.Next(), obj.Next(), obj.Next(), obj.Next(), obj.HasNext(), obj.Next(), obj.HasNext())
 }
 
 
