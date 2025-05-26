be34fe4a39336a411b33e387161ecfe90fba1a09
Eugene Kalashnikov <eugene.kalashnikov@fadv.com>
2025-05-26 07:17:18 +0200

    up

--- leetcode/1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.js
+++ leetcode/1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.js
@@ -11,44 +11,47 @@
  * @return {number}
  */
 var minReorder = function(n, connections) {
-    const directedGraph = new Map()
     const undirectedGraph = new Map()
 
-    for(let i = 0; i < n; i++) {
-        directedGraph.set(i, [])
+    for (let i = 0; i < n; i++) {
         undirectedGraph.set(i, [])
     }
 
-    for(let i = 0; i < connections.length; i++) {
+    const roads = new Set()
+    const getHash = (from, to) => `${from},${to}`
+
+    for (let i = 0; i < connections.length; i++) {
         const [from, to] = connections[i]
 
-        directedGraph.set(from, [...directedGraph.get(from), to])
         undirectedGraph.set(from, [...undirectedGraph.get(from), to])
         undirectedGraph.set(to, [...undirectedGraph.get(to), from])
+        roads.add(getHash(from, to))
     }
 
-    console.log(directedGraph)
-    console.log(undirectedGraph)
+    // console.log(undirectedGraph)
 
-    const visited = new Set()
     let swapCount = 0
+    const visited = new Array(n).fill(false)
+
+    visited[0] = true
+
     const dfs = (node) => {
         const neighbours = undirectedGraph.get(node)
 
-        console.log()
-        console.log(node, neighbours)
-
-        for(let i = 0; i < neighbours.length; i++) {
+        for (let i = 0; i < neighbours.length; i++) {
             const neighbour = neighbours[i]
-            console.log(node, neighbour, directedGraph.get(neighbour))
-            
-            if(!directedGraph.get(neighbour).includes(node)) {
-                swapCount++
-                console.log(`swap`, swapCount)
-            }
 
-            if(!visited.has(node)) {
-                visited.add(node)
+            if(!visited[neighbour]) {
+                visited[neighbour] = true
+
+                const wrongDirection = getHash(node, neighbour)
+
+                if(roads.has(wrongDirection)) {
+                    swapCount++
+
+                    // console.log(`swap`, wrongDirection, node, neighbour, swapCount)
+                }
+
                 dfs(neighbour)
             }
         }
@@ -56,25 +59,30 @@ var minReorder = function(n, connections) {
 
     dfs(0)
 
-
     return swapCount
 };
 // @lc code=end
 
 {
-    const n = 3
+    const n = 5
     const matrix = [
         [1,0],
-        [2,0]
+        [1,2],
+        [3,2],
+        [3,4]
     ]
 
     const answer = minReorder(n, matrix)
-    const expected = 0
+    const expected = 2
 
     console.log(answer === expected, answer)
 }
 
-return;
+/*
+0<-1->2<-3->4
+
+*/
+
 
 {
     const n = 6
@@ -105,6 +113,14 @@ return;
 3: [1, 2]
 4: [0, 5]
 5: [4]
+
+roads
+[0,1]
+[1,3]
+[2,3]
+[4,0]
+[4,5]
+
 
 to: 0 from: 1
 swap
@@ -227,6 +243,19 @@ swap
 }
 
 
+
+{
+    const n = 3
+    const matrix = [
+        [1,0],
+        [2,0]
+    ]
+
+    const answer = minReorder(n, matrix)
+    const expected = 0
+
+    console.log(answer === expected, answer)
+}
 
 {
     const n = 3
