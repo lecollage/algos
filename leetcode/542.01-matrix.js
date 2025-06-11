47b1a3da7587d8c18b452cd00ebfc511995e7da4
Eugene Kalashnikov <eugene.kalashnikov@fadv.com>
2025-06-11 19:23:07 +0200

    UP

---
+++ leetcode/542.01-matrix.js
@@ -0,0 +1,198 @@
+// @lc code=start
+class Node {
+  next;
+  value;
+
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class MyQueue {
+  _head = null;
+  _tail = null;
+
+  length = 0;
+
+  constructor() {}
+
+  push(value) {
+    const node = new Node(value);
+
+    if (!this._head) {
+      this._head = node;
+      this._tail = node;
+      this.length++;
+
+      return;
+    }
+
+    this._tail.next = node;
+    this._tail = this._tail.next;
+    this.length++;
+  }
+
+  pop() {
+    if (!this._head) {
+      return null;
+    }
+
+    const node = this._head;
+
+    if (this._tail === this._head) {
+      this._tail = null;
+      this._head = null;
+    } else {
+      this._head = this._head.next;
+    }
+
+    this.length--;
+
+    return node.value;
+  }
+}
+
+/*
+ * @lc app=leetcode id=542 lang=javascript
+ *
+ * [542] 01 Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function (mat) {
+  const explore = (startI, startJ) => {
+    const queue = new MyQueue();
+
+    queue.push({ i: startI, j: startJ, distance: 0 });
+
+    const directions = [
+      [-1, 0],
+      [1, 0],
+      [0, -1],
+      [0, 1],
+    ];
+    const isValid = (i, j) => {
+      if (i < 0 || i >= mat.length) {
+        return false;
+      }
+
+      if (j < 0 || j >= mat[i].length) {
+        return false;
+      }
+
+      return true;
+    };
+
+    while (queue.length) {
+      const { i, j, distance } = queue.pop();
+
+      //   console.log(i, j);
+
+      if (mat[i][j] === 0) {
+        return distance;
+      }
+
+      directions.forEach(([di, dj]) => {
+        const newI = i + di;
+        const newJ = j + dj;
+
+        if (isValid(newI, newJ)) {
+          queue.push({ i: newI, j: newJ, distance: distance + 1 });
+        }
+      });
+    }
+  };
+
+  for (let i = 0; i < mat.length; i++) {
+    for (let j = 0; j < mat[i].length; j++) {
+      if (mat[i][j] === 1) {
+        const distance = explore(i, j);
+
+        mat[i][j] = distance;
+      }
+    }
+  }
+
+  return mat;
+};
+// @lc code=end
+
+{
+  const matrix = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ];
+
+  const expected = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ];
+
+  const answer = updateMatrix(matrix);
+
+  console.log(answer === expected, answer);
+}
+
+{
+  const matrix = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 1],
+  ];
+
+  const expected = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 2, 1],
+  ];
+
+  const answer = updateMatrix(matrix);
+
+  console.log(answer === expected, answer);
+}
+
+{
+  const matrix = [
+    [0, 0, 0, 0],
+    [0, 1, 0, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+  ];
+
+  const expected = [
+    [0, 0, 0, 0],
+    [0, 1, 0, 1],
+    [1, 2, 1, 2],
+    [2, 3, 2, 3],
+  ];
+
+  const answer = updateMatrix(matrix);
+
+  console.log(answer === expected, answer);
+}
+
+{
+  const matrix = [
+    [0, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+  ];
+
+  const expected = [
+    [0, 1, 2, 1],
+    [1, 2, 3, 1],
+    [2, 3, 4, 2],
+    [2, 3, 2, 3],
+  ];
+
+  const answer = updateMatrix(matrix);
+
+  console.log(answer === expected, answer);
+}
